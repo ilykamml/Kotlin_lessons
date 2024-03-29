5cbb420ff52fe9e3fcda20886b43b023cc7bc912
Ilya Molokovich <im2004v@gmail.com>
2024-03-29 13:01:12 +0700

    Lesson 6 - ArraysListsSets.kt in work

---
+++ src/main/kotlin/ru/ilyamnl/lessons/ArraysListsSets.kt
@@ -0,0 +1,12 @@
+package ru.ilyamnl.lessons
+
+fun main() {
+
+    // int[] arr - jaba
+
+    val arr: Array<String> = arrayOf("1", "2", "3") // массив в котлине
+    arr[0] = "3"
+    println(arr[0])
+    println(arr)
+
+}
