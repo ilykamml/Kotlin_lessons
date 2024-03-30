c9ef9de0363a153df6b325f362f6fcc2e4fb558a
Ilya Molokovich <im2004v@gmail.com>
2024-03-30 23:58:38 +0700

    Homework 6 - ArraysListsSets.kt In work

---
+++ src/main/kotlin/ru/ilyamnl/homework/ArraysListsSets.kt
@@ -0,0 +1,131 @@
+package ru.ilyamnl.homework
+
+fun main() {
+
+    // ARRAYS
+
+    val arr1: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+
+    val arr2: Array<String?> = arrayOfNulls<String>(10)
+
+    val arr3 = DoubleArray(5) { index -> (index * 2).toDouble() }
+
+    val arr4: IntArray = intArrayOf(3, 4, 6, 33, 8)
+
+    for (i in arr4.indices) arr4[i] *= 3
+
+    val arr5: Array<String?> = arrayOfNulls(3)
+
+    arr5[1] = "Hello World"
+    arr5[2] = "Bye-bye world"
+
+    val arr6: IntArray = intArrayOf(1, 3, 6, 3, 7, 43, 4, 6, 4)
+    val arr7 = IntArray(arr6.size)
+    var index = 0
+
+    for (i in arr6) arr7[index++] = i
+
+    val arr8 = intArrayOf(14, 21, 53)
+    val arr9 = intArrayOf(7, 12, 24)
+    val arr10 = IntArray(3)
+
+    for (i in arr8.indices) arr10[i] = arr8[i] - arr9[i]
+
+    val arr11 = arrayOf(3, 4, 3, 123, 67, 5, 1233)
+
+    for (i in arrayOf(3, 5, 3, 6, 7, 9, 23, 4)) println(i)
+
+    println("\n\n")
+
+    // LISTS
+
+    val list1: List<Int> = listOf()
+
+    val list2 = listOf("Hello", "World", "Kotlin")
+
+    val list3 = MutableList<Int>(5) { i -> i * i + i + 15 * (i + 10) }.also {
+
+        it.addAll(listOf(658, 775, 282))
+    }
+
+    val list4: MutableList<String> = mutableListOf("Hello", "World")
+
+    list4.add("Kotlin")
+
+    for (i in list3) println(i)
+
+    println(list4[1])
+
+//    list3[2] = 10
+
+    val list5 = listOf(493, 618, 125, 431, 482, 648, 234, 295, 568)
+
+    println(simpleListSort(list5))
+
+    val list6 = list3.toList()
+
+    val list7 = list5 + list6
+
+    val list8 = mutableListOf<Int>().apply {
+        addAll(list5)
+        addAll(list6)
+    }
+
+    val list9 = mutableListOf<Int>().apply {
+        for (i in list5) this.add(i)
+        for (i in list6) this.add(i)
+    }
+
+    println("$list7\n$list8\n$list9")
+
+    list9.apply {
+
+        var max: Int = this[0]
+        var min: Int = this[0]
+
+        for (i in this) {
+            if (i > max) max = i
+            if (i < min) min = i
+        }
+
+        println("\nmin = $min\nmax = $max")
+
+    }
+
+    println(list9.filter { it % 2 == 0 })
+
+}
+
+fun findIndex(arr: Array<Int>, value: Int): Int {
+
+    for (i in arr.indices) if (arr[i] == value) return i
+    return -1
+
+}
+
+fun findIndexStr(arr: Array<String>, value: String): Int {
+
+    for (i in arr.indices) if (arr[i] == value) return i
+    return -1
+
+}
+
+fun simpleListSort(list: List<Int>): List<Int> {
+
+    val tempList = list.toMutableList()
+
+    for (i in tempList.indices) {
+        for (j in i+1..<tempList.size) {
+
+            if (tempList[j] < tempList[i]) {
+                val tmp = tempList[j]
+                tempList[j] = tempList[i]
+                tempList[i] = tmp
+            }
+
+        }
+    }
+
+    return tempList.toList()
+
+}
