fdc47819940118a2c21e949a415f1ca0be8e025a
Ilya Molokovich <im2004v@gmail.com>
2024-04-05 20:18:43 +0700

    Homework 24 - CerealStorageImplTest.kt Also created interface CerealStorage and class CerealStorageImpl

---
+++ src/main/kotlin/ru/ilyamnl/homework/CerealStorageImpl.kt
@@ -0,0 +1,120 @@
+package ru.ilyamnl.homework
+
+class CerealStorageImpl(containerCapacityArg: Float, storageCapacityArg: Float) : CerealStorage {
+
+    /**
+     * Map контейнеров
+     */
+    private val containers = mutableMapOf<Cereal, Float>()
+
+    /**
+     * Объём одного контейнера
+     * @throws IllegalArgumentException если значение отрицательное при инициализации
+     */
+    override val containerCapacity: Float = containerCapacityArg.also {
+        require(it >= 0) { "Container capacity must be not negative" }
+    }
+
+    /**
+     * Совокупный объём хранилища
+     * @throws IllegalArgumentException если значение меньше, чем [containerCapacity] при инициализации
+     */
+    override val storageCapacity: Float = storageCapacityArg.also {
+        require(it >= containerCapacity) { "Storage capacity must be at least equal to the container's capacity" }
+    }
+
+    /**
+     * @param cereal крупа для добавления в контейнер
+     * @param amount количество добавляемой крупы
+     * @return количество оставшейся крупы если контейнер заполнился
+     * @throws IllegalArgumentException если передано отрицательное значение
+     * @throws IllegalStateException если хранилище не позволяет разместить ещё один контейнер для новой крупы
+     */
+    override fun addCereal(cereal: Cereal, amount: Float): Float {
+        require(amount >= 0) { "Cereal amount must be not negative" }
+
+        // создание контейнера
+        if (!containers.containsKey(cereal)) {
+            check(!(storageCapacity - containers.size * containerCapacity < containerCapacity))
+            { "There is no place to place the container" }
+            containers[cereal] = 0f
+        }
+
+        // заполнение контейнера
+        val freeSpace = getSpace(cereal)
+        containers[cereal] = if (freeSpace > amount) containers.getValue(cereal) + amount
+                            else containerCapacity
+        return if (freeSpace > amount) 0f else amount - freeSpace
+
+    }
+
+    /**
+     * @param cereal крупа, которую нужно взять из контейнера
+     * @param amount количество крупы
+     * @return количество полученной крупы или остаток, если было меньше
+     * @throws IllegalArgumentException если передано отрицательное значение
+     */
+    override fun getCereal(cereal: Cereal, amount: Float): Float {
+        require(amount >= 0) { "Cereal amount must be not negative" }
+
+        val remainingCereal = getAmount(cereal)
+        if (remainingCereal < amount) {
+            containers[cereal] = 0f
+            return remainingCereal
+        } else {
+            containers[cereal] = containers.getValue(cereal) - amount
+            return amount
+        }
+    }
+
+    /**
+     * @param cereal уничтожает пустой контейнер
+     * @return true если контейнер уничтожен и false если контейнер не пуст
+     */
+    override fun removeContainer(cereal: Cereal): Boolean {
+        when {
+            !containers.containsKey(cereal) -> {
+                return true
+            }
+            containers[cereal] == 0f -> {
+                containers.remove(cereal)
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+
+    /**
+     * @param cereal крупа, количество которой нужно узнать
+     * @return количество крупы, которое хранится в контейнере
+     */
+    override fun getAmount(cereal: Cereal): Float {
+        return containers.getOrDefault(cereal, 0f)
+    }
+
+    /**
+     * @param cereal крупа, для которой нужно проверить доступное место в контейнере
+     * @return количество крупы, которое может вместить контейнер с учётом его текущей заполненности
+     */
+    override fun getSpace(cereal: Cereal): Float {
+        return if (containers.containsKey(cereal)) {
+            containerCapacity - getAmount(cereal)
+        } else {
+            0f
+        }
+    }
+
+    /**
+     * @return текстовое представление
+     */
+    override fun toString(): String {
+        var result = "In our containers:"
+        containers.toSortedMap().forEach {
+            result += "\n${it.key.getLocal()}: ${"%.2f".format(it.value)} tons"
+        }
+        return result
+    }
+
+}
---
+++ src/test/kotlin/ru/ilyamnl/homework/CerealStorageImplTest.kt
@@ -0,0 +1,194 @@
+package ru.ilyamnl.homework
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.assertEquals
+
+class CerealStorageImplTest {
+    private val defaultContainerManager: CerealStorage = CerealStorageImpl(20f, 60f)
+    @Test
+    fun getContainerCapacity() {
+        Assertions.assertEquals(20f, defaultContainerManager.containerCapacity,
+            "Container capacity must be 20f")
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException if container capacity negative`() {
+        Assertions.assertThrows(IllegalArgumentException::class.java)
+            { CerealStorageImpl(-20f, 60f) }
+    }
+
+    @Test
+    fun `should not throw IllegalArgumentException if container capacity negative`() {
+        Assertions.assertDoesNotThrow { CerealStorageImpl(0f, 60f) }
+    }
+
+    @Test
+    fun getStorageCapacity() {
+        Assertions.assertEquals(60f, defaultContainerManager.storageCapacity,
+            "Container capacity must be 60f")
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException if storage capacity lower than storage capacity`() {
+        Assertions.assertThrows(IllegalArgumentException::class.java)
+        { CerealStorageImpl(60f, 20f) }
+    }
+
+    @Test
+    fun `should not throw IllegalArgumentException if storage capacity lower than storage capacity`() {
+        Assertions.assertDoesNotThrow { CerealStorageImpl(20f, 20f) }
+    }
+
+    @Test
+    fun addCereal() {
+        defaultContainerManager.addCereal(Cereal.RICE, 12f)
+        Assertions.assertEquals(12f, defaultContainerManager.getAmount(Cereal.RICE))
+    }
+
+    @Test
+    fun `add extra cereal and get some back`() {
+        defaultContainerManager.addCereal(Cereal.RICE, 18f)
+        defaultContainerManager.addCereal(Cereal.PEAS, 12f)
+        defaultContainerManager.addCereal(Cereal.BULGUR, 9f)
+        Assertions.assertEquals(5f, defaultContainerManager.addCereal(Cereal.RICE, 7f), 0.1f)
+    }
+
+    @Test
+    fun `add four containers and get back exception`() {
+        defaultContainerManager.addCereal(Cereal.RICE, 18f)
+        defaultContainerManager.addCereal(Cereal.PEAS, 12f)
+        defaultContainerManager.addCereal(Cereal.BULGUR, 9f)
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            defaultContainerManager.addCereal(Cereal.MILLET, 1f)
+        }
+    }
+
+    @Test
+    fun `mound the cereal end to end and don't get an exception`() {
+        Assertions.assertDoesNotThrow { defaultContainerManager.addCereal(Cereal.RICE, 20f) }
+    }
+
+    @Test
+    fun `bring a negative amount of cereal`() {
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            defaultContainerManager.addCereal(Cereal.RICE, -0.1f)
+        }
+    }
+
+    @Test
+    fun getCereal() {
+        defaultContainerManager.addCereal(Cereal.RICE, 15f)
+        Assertions.assertEquals(10.4f, defaultContainerManager.getCereal(Cereal.RICE, 10.4f))
+        { "It should be 10.4f taken from container" }
+    }
+
+    @Test
+    fun `get negative amount of cereal`() {
+        defaultContainerManager.addCereal(Cereal.RICE, 15f)
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            defaultContainerManager.getCereal(Cereal.RICE, -1f)
+        }
+    }
+
+    @Test
+    fun `get cereal from non exist container`() {
+        Assertions.assertEquals(0f, defaultContainerManager.getCereal(Cereal.RICE, 12f))
+        { "Container doesnt exists. Should be 0f" }
+    }
+
+    @Test
+    fun `Get more than you really have`() {
+        defaultContainerManager.addCereal(Cereal.RICE, 15f)
+        Assertions.assertEquals(15f, defaultContainerManager.getCereal(Cereal.RICE, 20f))
+        { "15f - that's all you can get from here." }
+    }
+
+    @Test
+    fun removeContainer() {
+        defaultContainerManager.addCereal(Cereal.RICE, 0f)
+        Assertions.assertTrue(defaultContainerManager.removeContainer(Cereal.RICE))
+    }
+
+    @Test
+    fun `remove container after work`() {
+        defaultContainerManager.addCereal(Cereal.PEAS, 19f)
+        defaultContainerManager.addCereal(Cereal.PEAS, 0.5f)
+        defaultContainerManager.getCereal(Cereal.PEAS, 10.25f)
+        defaultContainerManager.addCereal(Cereal.PEAS, 0.75f)
+        defaultContainerManager.getCereal(Cereal.PEAS, 10f)
+        Assertions.assertTrue(defaultContainerManager.removeContainer(Cereal.PEAS))
+    }
+
+    @Test
+    fun `bring a new container to someone's place`() {
+        defaultContainerManager.addCereal(Cereal.RICE, 10f)
+        defaultContainerManager.addCereal(Cereal.PEAS, 5f)
+        defaultContainerManager.addCereal(Cereal.BULGUR, 19f)
+        defaultContainerManager.getCereal(Cereal.RICE, 10f)
+        defaultContainerManager.removeContainer(Cereal.RICE)
+        defaultContainerManager.addCereal(Cereal.MILLET, 12f)
+        Assertions.assertEquals(12f, defaultContainerManager.getAmount(Cereal.MILLET))
+        { "should work" }
+    }
+
+    @Test
+    fun `remove doesnt exists container`() {
+        Assertions.assertTrue(defaultContainerManager.removeContainer(Cereal.RICE))
+    }
+
+    @Test
+    fun `dont remove filled container`() {
+        defaultContainerManager.addCereal(Cereal.RICE, 0.01f)
+        Assertions.assertFalse(defaultContainerManager.removeContainer(Cereal.RICE))
+    }
+
+    @Test
+    fun getAmount() {
+        defaultContainerManager.addCereal(Cereal.RICE, 19.4f)
+        Assertions.assertEquals(19.4f, defaultContainerManager.getAmount(Cereal.RICE))
+        { "Rice should be 19.4f" }
+    }
+
+    @Test
+    fun `get amount with overflow`() {
+        defaultContainerManager.addCereal(Cereal.BULGUR, 45f)
+        Assertions.assertEquals(20f, defaultContainerManager.getAmount(Cereal.BULGUR))
+        { "Bulgur should be 20f" }
+    }
+
+    @Test
+    fun `get amount if field doesnt exists`() {
+        Assertions.assertEquals(0f, defaultContainerManager.getAmount(Cereal.RICE))
+        { "Rice should be 0" }
+    }
+
+    @Test
+    fun getSpace() {
+        defaultContainerManager.addCereal(Cereal.BUCKWHEAT, 19.5f)
+        Assertions.assertEquals(0.5f, defaultContainerManager.getSpace(Cereal.BUCKWHEAT))
+        { "Buckwheat should be 0.5f" }
+    }
+
+    @Test
+    fun `get space if field doesnt exists`() {
+        Assertions.assertEquals(0f, defaultContainerManager.getSpace(Cereal.RICE))
+        { "Rice should be 0" }
+    }
+
+    @Test
+    fun `get space if field filled in`() {
+        defaultContainerManager.addCereal(Cereal.RICE, 20f)
+        Assertions.assertEquals(0f, defaultContainerManager.getSpace(Cereal.RICE))
+        { "Rice should be 0" }
+    }
+
+    @Test
+    fun testToString() {
+        defaultContainerManager.addCereal(Cereal.RICE, 15f)
+        defaultContainerManager.addCereal(Cereal.BUCKWHEAT, 1.3f)
+        Assertions.assertEquals("In our containers:\nГречка: 1,30 tons\nРис: 15,00 tons"
+            , defaultContainerManager.toString())
+    }
+}
